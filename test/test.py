3f4706a26dd8cc83a81a5d0a65067a8c82a99fe8
flatearther666 <149151463+flatearther666@users.noreply.github.com>
2024-10-07 00:01:41 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -132,6 +132,7 @@ st.write("* A function named `bar_chart` is defined in this code, and it accepts
 
 #Line Graph
 st.markdown("## :red[**Line Graph**] - Aguas, Ynikko Arzee Neo D.")
+def line_chart(df):
 df ['Purchase Date'] = pd.to_datetime(df['Purchase Date'])
 
 y_axis_column = 'Quantity'
@@ -150,6 +151,7 @@ st.pyplot(plt)
 plt.clf()
 plt.show()
 
+line_chart(df)
 st.write("* This code snippet handles a DataFrame df by first using `pd.to_datetime()` to convert the 'Purchase Date' column into a datetime format. The y_axis_column is then set to 'Quantity,' and the data is grouped by 'Purchase Date' with the groupby method being used to total the amounts for each date. Matplotlib is used to build a line graph that plots the grouped data's values (quantities) against its index (dates), with the line color set to red. A title, labeled axes, rotated x-axis labels for easier reading, and a grid for better presentation are added to the customized graph. Finally, `plt.show()` is used to display the graph.")
 
 
